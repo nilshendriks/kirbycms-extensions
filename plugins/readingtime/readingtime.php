2a223d02792cd259f079d2d27910f9cfe379c942
Nils Hendriks <nils@nilshendriks.com>
2012-05-19 11:31:11 +0900

    Changed readingtime plugin
    Now counts words after link href values have been stripped with kirbytext
    
    Signed-off-by: Nils Hendriks <nils@nilshendriks.com>

--- plugins/readingtime/readingtime.php
+++ plugins/readingtime/readingtime.php
@@ -1,22 +1,22 @@
 <?php
 
-/* 
+/*
  * Reading Time
- * 
- * A simple plugin that estimates the reading 
+ *
+ * A simple plugin that estimates the reading
  * time for any text
  *
- * Sample Usage: 
- * 
+ * Sample Usage:
+ *
  * <?php echo readingtime($page->text()) ?>
- * 
+ *
  * Author: Roy Lodder <http://roylodder.com>
  * Contributor: Bastian Allgeier <http://getkirby.com>
  *
  */
 
 function readingtime($content, $params=array()) {
-    
+
   $defaults = array(
     'minute'  => 'minute',
     'minutes' => 'minutes',
@@ -24,9 +24,12 @@ function readingtime($content, $params=array()) {
     'seconds' => 'seconds',
     'format'  => '{minutesCount} {minutesLabel}, {secondsCount} {secondsLabel}'
   );
-  
+
   $options = array_merge($defaults, $params);
-  $words   = str_word_count(strip_tags($content));
+  //$words   = str_word_count(strip_tags($content));
+
+  $words = kirbytext($content);
+  $words   = str_word_count(strip_tags($words));
 
   $minutesCount = floor($words / 200);
   $secondsCount = floor($words % 200 / (200 / 60));
@@ -36,8 +39,8 @@ function readingtime($content, $params=array()) {
   $replace  = array(
     'minutesCount' => $minutesCount,
     'minutesLabel' => $minutesLabel,
-    'secondsCount' => $secondsCount, 
-    'secondsLabel' => $secondsLabel, 
+    'secondsCount' => $secondsCount,
+    'secondsLabel' => $secondsLabel,
   );
 
   $result = $options['format'];
